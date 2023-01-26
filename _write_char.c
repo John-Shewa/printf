72a906f8964ef3eac527be3f3d0310386112349b
John-Shewa <eldi2002@gmail.com>
2023-01-26 11:52:47 -0800

    commit

---
+++ _write_char.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _write_char.c - writes the character c to stdout
+ * @c: the character to print
+ * Return: -1 if error, 1 if success
+ */
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
